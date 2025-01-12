8e27bb6ee30a011939b332dd11fadc32940e0b65
fr3zill@ <fb.info@mailo.com>
2025-01-12 22:29:52 +0100

    Finalisation simple de l'interface SwingDevice

--- text-device/src/main/java/eu/frezilla/tools/textdevice/SwingDevice.java
+++ text-device/src/main/java/eu/frezilla/tools/textdevice/SwingDevice.java
@@ -4,14 +4,19 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 public class SwingDevice {
     
     private final JFrame frame;
+    private final JTextArea jTextArea;
     
     public SwingDevice() {
         frame = new JFrame("SwingDevice");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        jTextArea = new JTextArea(20, 20);
         
         frame.setContentPane(createContentPane());
         frame.pack();
@@ -24,6 +29,12 @@ public class SwingDevice {
         
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.gridx = 0; gbc.gridy = 0; gbc.weightx = 1.0; gbc.weighty = 1.0; gbc.anchor = GridBagConstraints.CENTER; gbc.fill = GridBagConstraints.BOTH;
+        
+        JScrollPane scrollableTextArea = new JScrollPane(jTextArea);
+        scrollableTextArea.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        scrollableTextArea.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        
+        panel.add(scrollableTextArea, gbc);
         
         return panel;
     }
