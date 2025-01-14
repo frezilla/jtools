4fa1f81b4e7e53cef3512cc3fdd228d328fdbbfa
fr3zill@ <fb.info@mailo.com>
2025-01-14 22:48:27 +0100

    Ajout de Junit et initialisation du test

---
+++ memory/src/test/java/eu/frezilla/jtools/memory/test/BufferTest.java
@@ -0,0 +1,37 @@
+package eu.frezilla.jtools.memory.test;
+
+import eu.frezilla.jtools.memory.Buffer;
+import eu.frezilla.jtools.memory.BufferException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class BufferTest {
+
+    @Test
+    public void test() throws BufferException {
+        Buffer<String> buffer = new Buffer(5);
+
+        Assertions.assertTrue(buffer.isEmpty());
+        Assertions.assertFalse(buffer.isFull());
+
+        buffer.add("chaine1");
+        buffer.add("chaine2");
+
+        Assertions.assertFalse(buffer.isEmpty());
+        Assertions.assertFalse(buffer.isFull());
+
+        buffer.add("chaine3");
+        buffer.add("chaine4");
+        buffer.add("chaine5");
+        
+        Assertions.assertFalse(buffer.isEmpty());
+        Assertions.assertTrue(buffer.isFull());
+        
+        Assertions.assertEquals(buffer.removeFirst(), "chaine1");
+        Assertions.assertEquals(buffer.removeLast(), "chaine5");
+        
+        buffer.add("chaine6");
+        buffer.add("chaine7");
+    }
+
+}
