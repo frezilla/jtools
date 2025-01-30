c6306c7b108e4363cee274c7eb7e17bbf8ce7ee7
fr3zill@ <fb.info@mailo.com>
2025-01-30 22:17:38 +0100

    Développement du TextDevice à la sauce swing

--- text-device/src/main/java/eu/frezilla/jtools/textdevice/SwingDevice.java
+++ text-device/src/main/java/eu/frezilla/jtools/textdevice/SwingDevice.java
@@ -2,42 +2,81 @@ package eu.frezilla.jtools.textdevice;
 
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import java.io.PrintWriter;
+import java.io.Reader;
+import javax.swing.JComponent;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.SwingUtilities;
+import javax.swing.text.AbstractDocument;
 
-public class SwingDevice {
+public class SwingDevice extends JComponent implements TextDevice {
     
-    private final JFrame frame;
+    private final SwingDeviceDocumentFilter documentFilter;
     private final JTextArea jTextArea;
     
     public SwingDevice() {
-        frame = new JFrame("SwingDevice");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-        jTextArea = new JTextArea(20, 20);
-        
-        frame.setContentPane(createContentPane());
-        frame.pack();
-        frame.setVisible(true);
+        this(20, 80);
     }
     
-    private JPanel createContentPane() {
-        JPanel panel = new JPanel(new GridBagLayout());
-        panel.setLayout(new GridBagLayout());
+    public SwingDevice(int rows, int columns) {
+        documentFilter = new SwingDeviceDocumentFilter();
+        jTextArea = new JTextArea(rows, columns);
+        ((AbstractDocument) jTextArea.getDocument()).setDocumentFilter(documentFilter);
         
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.gridx = 0; gbc.gridy = 0; gbc.weightx = 1.0; gbc.weighty = 1.0; gbc.anchor = GridBagConstraints.CENTER; gbc.fill = GridBagConstraints.BOTH;
-        
-        JScrollPane scrollableTextArea = new JScrollPane(jTextArea);
-        scrollableTextArea.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-        scrollableTextArea.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-        
-        panel.add(scrollableTextArea, gbc);
-        
-        return panel;
+        createGUI();
     }
     
+    private void createGUI() {
+        SwingUtilities.invokeLater(() -> {
+            this.setLayout(new GridBagLayout());
+            GridBagConstraints gbc = new GridBagConstraints();
+            gbc.gridx = 0; gbc.gridy = 0; gbc.weightx = 1.0; gbc.weighty = 1.0; gbc.anchor = GridBagConstraints.CENTER; gbc.fill = GridBagConstraints.BOTH;
+        
+            JScrollPane scrollableTextArea = new JScrollPane(jTextArea);
+            scrollableTextArea.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+            scrollableTextArea.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        
+            this.add(scrollableTextArea, gbc);
+        });
+    }
+
+    @Override
+    public TextDevice printf(String fmt, Object... params) throws TextDeviceException {
+        String text = String.format(fmt, params);
+        
+        SwingUtilities.invokeLater(() -> {
+            jTextArea.append(text);
+            int position = jTextArea.getDocument().getStartPosition().getOffset();
+            documentFilter.setPromptPosition(position + text.length());
+        });
+        
+        return this;
+    }
+
+    @Override
+    public Reader reader() throws TextDeviceException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public String readLine(String fmt, Object... params) throws TextDeviceException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public char[] readPassword(String fmt, Object... params) throws TextDeviceException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
+    public TextDevice toTextDevice() {
+        return this;
+    }
+
+    @Override
+    public PrintWriter writer() throws TextDeviceException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+   
 }
---
+++ text-device/src/main/java/eu/frezilla/jtools/textdevice/SwingDeviceDocumentFilter.java
@@ -0,0 +1,47 @@
+package eu.frezilla.jtools.textdevice;
+
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.DocumentFilter;
+import lombok.Getter;
+
+final class SwingDeviceDocumentFilter extends DocumentFilter {
+    
+    @Getter private int promptPosition;
+    
+    public SwingDeviceDocumentFilter() {
+        this(0);
+    }
+    
+    public SwingDeviceDocumentFilter(int promptPosition) {
+        super();
+        setPromptPosition(promptPosition);
+    }
+    
+    @Override
+    public void insertString(final FilterBypass fb, final int offset, final String string, final AttributeSet attr) throws BadLocationException {
+        if (offset >= promptPosition) {
+            super.insertString(fb, offset, string, attr);
+        }
+    }
+
+    @Override
+    public void remove(final FilterBypass fb, final int offset, final int length) throws BadLocationException {
+        if (offset >= promptPosition) {
+            super.remove(fb, offset, length);
+        }
+    }
+
+    @Override
+    public void replace(final FilterBypass fb, final int offset, final int length, final String text, final AttributeSet attrs) throws BadLocationException {
+        if (offset >= promptPosition) {
+            super.replace(fb, offset, length, text, attrs);
+        }
+    }
+    
+    public void setPromptPosition(int promptPosition) {
+        if (promptPosition < 0) throw new IllegalArgumentException("La valeur du paramètre promptPosition n'est pas autorisée");
+        this.promptPosition = promptPosition;
+    }
+    
+}
