24cf93c1f66287df52699bad7183c597cb0a2083
fr3zill@ <fb.info@mailo.com>
2025-02-03 13:54:44 +0100

    Création de la classe

---
+++ text-device/src/main/java/eu/frezilla/jtools/textdevice/swing/SDPromptListener.java
@@ -0,0 +1,23 @@
+package eu.frezilla.jtools.textdevice.swing;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+final class SDPromptListener implements KeyListener {
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
