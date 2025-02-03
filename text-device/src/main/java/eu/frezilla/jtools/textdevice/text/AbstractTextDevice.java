9fd7cdda3b816951afeec7e9150e596aa9542cd1
fr3zill@ <fb.info@mailo.com>
2025-02-03 13:45:19 +0100

    Réorganisation du projet : création du package eu.frezilla.jtools.textdevice.text

---
+++ text-device/src/main/java/eu/frezilla/jtools/textdevice/text/AbstractTextDevice.java
@@ -0,0 +1,69 @@
+package eu.frezilla.jtools.textdevice.text;
+
+import eu.frezilla.jtools.textdevice.TextDevice;
+import eu.frezilla.jtools.textdevice.TextDeviceException;
+import java.io.PrintWriter;
+import java.io.Reader;
+
+abstract class AbstractTextDevice implements TextDevice {
+    
+    protected abstract void flushImpl() throws Exception;
+    
+    @Override
+    public TextDevice printf(String fmt, Object... params) throws TextDeviceException {
+        try {
+            printfImpl(fmt, params);
+            flushImpl();
+            return this;
+        } catch (Exception e) {
+            throw new TextDeviceException(e);
+        }
+    }
+    
+    protected abstract void printfImpl(String fmt, Object... params) throws Exception;
+        
+    @Override
+    public String readLine(String fmt, Object... params) throws TextDeviceException {
+        try {
+            return readLineImpl(fmt, params);
+        } catch (Exception e) {
+            throw new TextDeviceException(e);
+        }
+    }
+    
+    protected abstract String readLineImpl(String fmt, Object... params) throws Exception;
+
+    @Override
+    public char[] readPassword(String fmt, Object... params) throws TextDeviceException {
+        try {
+            return readPasswordImpl(fmt, params);
+        } catch (Exception e) {
+            throw new TextDeviceException(e);
+        }
+    }
+    
+    protected abstract char[] readPasswordImpl(String fmt, Object... params) throws Exception;
+
+    @Override
+    public Reader reader() throws TextDeviceException {
+        try {
+            return readerImpl();
+        } catch (Exception e) {
+            throw new TextDeviceException(e);
+        }
+    }
+    
+    protected abstract Reader readerImpl() throws Exception;
+
+    @Override
+    public PrintWriter writer() throws TextDeviceException {
+        try {
+            return writerImpl();
+        } catch (Exception e) {
+            throw new TextDeviceException(e);
+        }
+    }
+    
+    protected abstract PrintWriter writerImpl() throws Exception;
+    
+}
