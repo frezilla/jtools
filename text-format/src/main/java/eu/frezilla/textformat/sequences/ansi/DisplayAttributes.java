ce02d0cfcbc3f39fe882b9e08476754869cc05cc
fr3zill@ <fb.info@mailo.com>
2025-03-01 21:23:34 +0100

    Ajout de quelques éléments dans l'énumération

--- text-format/src/main/java/eu/frezilla/textformat/sequences/ansi/DisplayAttributes.java
+++ text-format/src/main/java/eu/frezilla/textformat/sequences/ansi/DisplayAttributes.java
@@ -1,12 +1,16 @@
 package eu.frezilla.textformat.sequences.ansi;
 
-public enum DisplayAttributes {
-    RESET(0);
-    
-    private final DisplayAttribute displayAttribute;
+import eu.frezilla.textformat.sequences.ansi.custom.ColorParameters;
+import lombok.Getter;
 
-    private DisplayAttributes(int n) {
-        this.displayAttribute = new DisplayAttribute(n);
+public enum DisplayAttributes {
+    RESET(0, null, "Reset or normal", "All attributes become turned off"),
+    BOLD(1, null, "Bold or increased intensity", "As with faint, the color change is a PC (SCO / CGA) invention");
+    
+    @Getter private final DisplayAttribute displayAttribute;
+
+    private DisplayAttributes(int n, ColorParameters colorParameters, String name, String note) {
+        this.displayAttribute = new DisplayAttribute(n, colorParameters, name, note);
     }
     
 }
