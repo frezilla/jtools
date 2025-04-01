5575c370eafa701a5d8430f20c19f354a202a3b8
fr3zill@ <fb.info@mailo.com>
2025-04-01 22:58:33 +0200

    Renommage des classes

---
+++ text-device/src/main/java/eu/frezilla/jtools/textdevice/swing/PlainTextSwingDevice.java
@@ -0,0 +1,81 @@
+package eu.frezilla.jtools.textdevice.swing;
+
+import eu.frezilla.jtools.textdevice.TextDevice;
+import eu.frezilla.jtools.textdevice.TextDeviceException;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import javax.swing.JComponent;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.SwingUtilities;
+import javax.swing.text.AbstractDocument;
+
+public class PlainTextSwingDevice extends JComponent implements TextDevice {
+    
+    private final JTextArea jTextArea;
+    private final PlainTextSwingDeviceEngine engine;
+    
+    public PlainTextSwingDevice() {
+        this(25, 80);
+    }
+    
+    public PlainTextSwingDevice(int rows, int columns) {
+        this(PlainTextSwingDeviceConfig.getBuilder().rows(rows).columns(columns).build());
+    }
+    
+    public PlainTextSwingDevice(PlainTextSwingDeviceConfig config) {
+        jTextArea = new JTextArea(config.getRows(), config.getColumns());
+        jTextArea.setBackground(config.getBackgroundColor());
+        jTextArea.setForeground(config.getForegroundColor());
+        jTextArea.setCaretColor(config.getCaretColor());
+        jTextArea.setCaretPosition(0);
+        jTextArea.setEditable(false);
+        
+        engine = new PlainTextSwingDeviceEngine(jTextArea);
+        
+        ((AbstractDocument) jTextArea.getDocument()).setDocumentFilter(engine);
+        jTextArea.addKeyListener(engine);
+        
+        createGUI();
+    }
+    
+    private void createGUI() {
+        super.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0; gbc.gridy = 0; gbc.weightx = 1.0; gbc.weighty = 1.0; gbc.anchor = GridBagConstraints.CENTER; gbc.fill = GridBagConstraints.BOTH;
+
+        JScrollPane scrollableTextArea = new JScrollPane(jTextArea);
+        scrollableTextArea.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        scrollableTextArea.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+
+        super.add(scrollableTextArea, gbc);
+    }
+
+    @Override
+    public TextDevice printf(String fmt, Object... params) throws TextDeviceException {
+        String text = String.format(fmt, params);
+        
+        SwingUtilities.invokeLater(() -> {
+            engine.write(text);
+        });
+        
+        return this;
+    }
+
+    @Override
+    public String readLine(String fmt, Object... params) throws TextDeviceException {
+        printf(fmt, params);
+        return engine.read();
+    }
+
+    @Override
+    public char[] readPassword(String fmt, Object... params) throws TextDeviceException {
+        printf(fmt, params);
+        return engine.readPassword().toCharArray();
+    }
+    
+    public TextDevice toTextDevice() {
+        return this;
+    }
+
+}
---
+++ text-device/src/main/java/eu/frezilla/jtools/textdevice/swing/PlainTextSwingDeviceEngine.java
@@ -0,0 +1,144 @@
+package eu.frezilla.jtools.textdevice.swing;
+
+import eu.frezilla.jtools.textdevice.TextDeviceException;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+import javax.swing.JTextArea;
+import javax.swing.SwingUtilities;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.DocumentFilter;
+
+final class PlainTextSwingDeviceEngine extends DocumentFilter implements KeyListener {
+    
+    private final char echoPassword;
+    private boolean enterPressed;
+    private final JTextArea jTextArea;
+    private int offset;
+    private StringBuilder passwordRecord;
+    private boolean passwordMode;
+    
+    public PlainTextSwingDeviceEngine(JTextArea jTextArea) {
+        this(jTextArea, '#');
+    }
+    
+    public PlainTextSwingDeviceEngine(JTextArea jTextArea, char echoPassword) {
+        this.echoPassword = echoPassword;
+        this.enterPressed = false;
+        this.jTextArea = Objects.requireNonNull(jTextArea);
+        if (offset < 0) throw new IllegalArgumentException("La valeur du paramètre offset n'est pas autorisée");
+        this.offset = 0;        
+        this.passwordMode = false;
+    }
+    
+    @Override
+    public void insertString(final FilterBypass fb, final int offset, final String string, final AttributeSet attr) throws BadLocationException {
+        if (offset >= this.offset) {
+            super.insertString(fb, offset, string, attr);
+        }
+    }    
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+            this.enterPressed = true;            
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) { 
+        // Do nothing 
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        if (passwordMode && !enterPressed) {
+            passwordRecord.append(e.getKeyChar());
+            
+            SwingUtilities.invokeLater(() -> {
+                jTextArea.append(String.valueOf(echoPassword));
+                try {
+                    jTextArea.getDocument().remove(jTextArea.getCaretPosition() - 2, 1);
+                } catch (BadLocationException ex) {
+                    // Do nothing
+                }
+            });
+            
+        }
+    }
+    
+    public String read() throws TextDeviceException {
+        String inputString = "";        
+        TextDeviceException exception = null;
+        
+        try {
+            jTextArea.setEditable(true);
+
+            while (!enterPressed) TimeUnit.MILLISECONDS.sleep(10);
+            
+            int caretPosition = jTextArea.getCaretPosition();
+            int length = (caretPosition - 1) - offset;
+            inputString = jTextArea.getDocument().getText(offset, length);
+            
+            offset = caretPosition;
+        } catch (BadLocationException | InterruptedException e) {
+            exception = new TextDeviceException("Erreur à la lecture de la saisie de l'utilisateur", e);
+            Thread.currentThread().interrupt();
+        } finally {
+            jTextArea.setEditable(false);
+            enterPressed = false;
+        }
+        
+        if (exception != null) throw exception;
+        
+        return inputString;
+    }
+    
+    public String readPassword() {
+        String inputString = "";
+        
+        try {
+            jTextArea.setEditable(true);
+            passwordMode = true;
+            passwordRecord = new StringBuilder();
+            
+            while (!enterPressed) TimeUnit.MILLISECONDS.sleep(10);
+        
+            inputString = passwordRecord.toString();
+        } catch (InterruptedException e) {  
+            Thread.currentThread().interrupt();            
+        } finally {
+            jTextArea.setEditable(false);
+            enterPressed = false;
+            passwordMode = false;
+        }
+        
+        return inputString;
+    }
+    
+    @Override
+    public void remove(final FilterBypass fb, final int offset, final int length) throws BadLocationException {
+        if (offset >= this.offset) {
+            super.remove(fb, offset, length);
+        }
+    }    
+    
+    @Override
+    public void replace(final FilterBypass fb, final int offset, final int length, final String text, final AttributeSet attrs) throws BadLocationException {
+        if (offset >= this.offset) {
+            super.replace(fb, offset, length, text, attrs);
+        }
+    }    
+    
+    public void write(String text) {
+        if (text == null) throw new IllegalArgumentException("text parameter is null");
+        
+        SwingUtilities.invokeLater(() -> {
+            jTextArea.append(text);
+            offset = offset + text.length();
+            jTextArea.setCaretPosition(offset);
+        });
+    }
+}
